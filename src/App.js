784984ea16e20d2dfa4d9c5d12efc7816e39286e
BethJW <bethanyjwilliamson16@gmail.com>
2024-01-27 18:28:45 +0000

    Added links to gtihub and netlify

--- src/App.js
+++ src/App.js
@@ -7,6 +7,12 @@ function App() {
   return (
     <div className="App">
       <Weather city="crewe" />
+      <div>
+        Coded by <a href="https://github.com/BethJW">Beth Williamson</a>,
+        open-source on{" "}
+        <a href="https://github.com/BethJW/weatherapp-react">Github</a> and
+        hosted on <a href="https://react-weather-bjw.netlify.app/">Netlify</a>
+      </div>
     </div>
   );
 }
